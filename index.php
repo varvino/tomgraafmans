28468476083979a55a8864ee0f6abd3f87421e7e
Tom Graafmans <graafmans.tom@gmail.com>
2020-06-21 19:54:21 +0200

    Frontpage: renamed introduction container to overview-container

--- index.php
+++ index.php
@@ -10,18 +10,19 @@
          <h2 class="hero__subtitle">Hallo, ik ben Tom</h2>
          <h1 class="hero__title">Een junior WordPress developer</h1>
       </div>
+
       <div class="overview-container">
          <div class="container">
-            <p class="introduction__text">Ik ben op zoek naar een nieuwe uitdaging in omgeving Amsterdam.</p>
-            <div class="margin-bottom--md">
-               <h3 class="title title--small">Portfolio</h3>
+            <p class="overview__text">Ik ben op zoek naar een nieuwe uitdaging in omgeving Amsterdam.</p>
+            <div class="overview__inner-container">
+               <h3 class="title--sm">Portfolio</h3>
                <p>Bekijk hier een overzicht van al mijn werk.</p>
-               <a href="<?php echo site_url(); ?>" class="link">Bekijk portfolio</a>
+               <a href="<?php echo site_url('projects'); ?>" class="button">Bekijk portfolio</a>
             </div>
-            <div class="margin-bottom--md">
-               <h3 class="title title--small">Curriculum Vitae</h3>
+            <div class="overview__inner-container">
+               <h3 class="title--sm">Curriculum Vitae</h3>
                <p>Voor een uitgebreidere kijk op mijn vaardigheden verwijs ik u naar mijn CV.</p>
-               <a href="<?php echo site_url(); ?>" class="link">Bekijk CV</a>
+               <a href="<?php echo site_url(); ?>" class="button">Bekijk CV</a>
             </div>
          </div>
       </div>
