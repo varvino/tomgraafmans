022eac0928a7a1e964eacf5ba072ed6a5b2cf83e
Tom Graafmans <graafmans.tom@gmail.com>
2020-06-21 19:55:50 +0200

    Archive: added default archive skeleton

--- archive.php
+++ archive.php
@@ -7,16 +7,20 @@
     <main class="container">
         <h1><?php post_type_archive_title(); ?></h1>
 
-        <?php if (have_posts()) :
-            while (have_posts()) :
-                the_post(); ?>
-                <article class="post">
-                    <h1 class="post__title"><?php the_title(); ?></h2>
-                        <div class="post__content"><?php the_excerpt(); ?></div>
-                </article>
-        <?php
-            endwhile;
-        endif; ?>
+        <?php if (have_posts()) : ?>
+            <ul class="archive-posts">
+                <?php while (have_posts()) : the_post(); ?>
+                    <li class="archive-post">
+                        <h3 class="archive-post__title"><?php the_title(); ?></h3>
+                        <?php if (the_excerpt()) : ?>
+                            <p class="archive-post__content"><?php the_excerpt(); ?></p>
+                        <?php endif; ?>
+                        <a href="<?php the_permalink(); ?>" class="button--archive-post">Bekijk</a>
+                    </li>
+                <?php
+                endwhile; ?>
+            </ul>
+        <?php endif; ?>
     </main>
 
     <?php get_template_part('template-parts/footer'); ?>
