75cc517b1a746970680e60a73c5f2be3294227c8
Tom Graafmans <graafmans.tom@gmail.com>
2020-06-21 19:44:23 +0200

    Footer: added social icons

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -1,6 +1,17 @@
 <footer>
     <div class="container">
-        <div class="copyright text--muted">
+            <div class="social__icons">
+                <a href="https://www.linkedin.com" class="social__icon">
+                    <img src="<?php echo get_theme_file_uri('/assets/img/icons/linkedin.svg') ?>" alt="Deze link leidt naar mijn persoonlijke LinkedIn-pagina." class="social__icon-image">
+                </a>
+                <a href="https://www.github.com" class="social__icon">
+                    <img src="<?php echo get_theme_file_uri('/assets/img/icons/github.svg') ?>" alt="Op mijn Github heeft u inzicht in hoe ik mijn thema's opzet." class="social__icon-image">
+                </a>
+                <a href="<?php echo site_url('contact-details') ?>" class="social__icon">
+                    <img src="<?php echo get_theme_file_uri('/assets/img/icons/file-text.svg') ?>" alt="Wilt u graag snel contact opnemen? Dat kan! Op deze pagina vindt u mijn contactgevens." class="social__icon-image">
+                </a>
+            </div>
+        </div>
             Copyright <?php echo date('Y'); ?> &mdash; Tom Graafmans
         </div>
     </div>
