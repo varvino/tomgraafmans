21e16557f42698d550ecc9db4bb483ac37e6227b
Tom Graafmans <graafmans.tom@gmail.com>
2020-06-24 22:26:55 +0200

    Theme: fixed bad paths

--- template-parts/components/navigation.php
+++ template-parts/components/navigation.php
@@ -1,5 +1,5 @@
 <div class="hamburger js-hamburger">
-    <img class="hamburger__icon" src="<?php echo get_theme_file_uri('assets/img/icons/hamburger.svg'); ?>" alt="<?php bloginfo('name'); ?>">
+    <img class="hamburger__icon" src="<?php echo get_theme_file_uri('assets/img/icons/utility/hamburger.svg'); ?>" alt="<?php bloginfo('name'); ?>">
 </div>
 
 <nav class="navmenu js-navmenu">
