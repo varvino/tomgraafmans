daddfd315d40f3b6f5f2b2f9c42e4e68c6698c48
Tom Graafmans <graafmans.tom@gmail.com>
2020-06-24 21:40:29 +0200

    Project Page: if no images are selected, don't show carousel. Text changes.

--- single-project.php
+++ single-project.php
@@ -12,46 +12,48 @@
                     <h1 class="post__title"><?php the_title(); ?></h1>
                     <div class="post__content"><?php the_content(); ?></div>
                 </div>
-                <div class="owl-carousel owl-theme margin--lg">
-                    <?php if (have_rows('portfolio_gallery')) : ?>
+
+                <?php if (have_rows('portfolio_gallery')) : ?>
+                    <div class="owl-carousel owl-theme margin--lg">
                         <?php while (have_rows('portfolio_gallery')) : the_row(); ?>
                             <div class="carousel__item">
                                 <img src="<?php the_sub_field('image'); ?>" class="carousel__image">
                             </div>
                         <?php endwhile;
                     else : ?>
+                        <div class="margin-bottom--md"></div>
                     <?php endif; ?>
-                </div>
-        <?php
+                    </div>
+            <?php
             endwhile;
         endif; ?>
 
-        <?php $portfolio_items = new WP_Query(array(
-            'post_type' => 'project',
-            'posts_per_page' => 2,
-            'post_status' => 'publish',
-            'orderby' => 'rand',
-            'order' => 'ASC',
-            'post__not_in' => array(get_the_ID())
-        )); ?>
+            <?php $portfolio_items = new WP_Query(array(
+                'post_type' => 'project',
+                'posts_per_page' => 2,
+                'post_status' => 'publish',
+                'orderby' => 'rand',
+                'order' => 'ASC',
+                'post__not_in' => array(get_the_ID())
+            )); ?>
 
-        <?php if ($portfolio_items > 0) : ?>
-            <div class="more-projects-containere">
-                <h3>Meer projecten</h3>
-                <ul class="archive-posts archive-posts--<?php echo get_post_type(); ?>">
-                    <?php while ($portfolio_items->have_posts()) : $portfolio_items->the_post(); ?>
-                        <li class="archive-post archive-post--<?php echo get_post_type(); ?>">
-                            <a href="<?php the_permalink(); ?>" class="archive-post__link">
-                                <?php the_post_thumbnail('large', ['class' => 'archive-post__image']); ?>
-                            </a>
-                        </li>
-                    <?php endwhile; ?>
-                </ul>
-                <a href="<?php echo site_url('projects'); ?>" class="button margin-top--md">Bekijk meer projecten</a>
-            </div>
-        <?php endif; ?>
+            <?php if ($portfolio_items > 0) : ?>
+                <div class="more-projects-container">
+                    <h3>Meer projecten</h3>
+                    <ul class="archive-posts archive-posts--<?php echo get_post_type(); ?>">
+                        <?php while ($portfolio_items->have_posts()) : $portfolio_items->the_post(); ?>
+                            <li class="archive-post archive-post--<?php echo get_post_type(); ?>">
+                                <a href="<?php the_permalink(); ?>" class="archive-post__link">
+                                    <?php the_post_thumbnail('large', ['class' => 'archive-post__image']); ?>
+                                </a>
+                            </li>
+                        <?php endwhile; ?>
+                    </ul>
+                    <a href="<?php echo site_url('projects'); ?>" class="button margin-top--md">Bekijk alle projecten</a>
+                </div>
+            <?php endif; ?>
 
-        <?php wp_reset_query(); ?>
+            <?php wp_reset_query(); ?>
 
     </main>
 
