bc56cebdab6a2c2fdf79d58e1e4fb52abde429b9
Tom Graafmans <graafmans.tom@gmail.com>
2020-07-10 09:36:37 +0200

    Theme: redirect if people land on author page

--- functions.php
+++ functions.php
@@ -207,3 +207,17 @@ function portfolio_inits()
 }
 
 add_action('wp_footer', 'portfolio_inits', 999);
+
+function my_custom_disable_author_page()
+{
+	global $wp_query;
+
+	if (is_author()) {
+		// Redirect to homepage, set status to 301 permenant redirect. 
+		// Function defaults to 302 temporary redirect. 
+		wp_redirect(get_option('home'), 301);
+		exit;
+	}
+}
+
+add_action('template_redirect', 'my_custom_disable_author_page');
